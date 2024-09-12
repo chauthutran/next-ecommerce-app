f67aa8e425a9b3741b3a16dd3453e58bb9e6f4cb
chauthutran <cthutran@gmail.com>
2024-09-12 23:35:07 +0900

    Update Header.tsx

--- app/ui/layout/Header.tsx
+++ app/ui/layout/Header.tsx
@@ -4,8 +4,10 @@ import { JSONObject } from "@/lib/definations";
 import ProductSearch from "../product/ProductSearchInput";
 import { useMainUi } from "@/contexts/MainUiContext";
 import * as Constant from "@/lib/constants";
-import { FaShopify } from "react-icons/fa";
 import { SiLeaflet } from "react-icons/si";
+import { SiReactquery } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { SiWearos } from "react-icons/si";
 
 
 export default function Header({onSearchSuccess, onSearchError}: {onSearchSuccess: (data: JSONObject[]) => void, onSearchError: (msg: string) => void}) {
@@ -15,14 +17,21 @@ export default function Header({onSearchSuccess, onSearchError}: {onSearchSucces
     return (
         <header className="grid grid-cols-1 md:grid-cols-2 px-4 py-3 border-b-2 border-gray-200 gap-4">
             <div className="text-2xl flex space-x-3 cursor-pointer items-center" onClick={() => setMainPage(Constant.PAGE_HOME)}>
-                {/* <div className="rounded-full bg-[#ADD8E6] text-[#00246B]"><FaShopify className="size-10" /></div> */}
-                <div className="rounded-full pt-1 bg-[#e2e6ad] text-[#6b6800]"><FaShopify className="size-10" /></div>
+                {/* <div className="rounded-full bg-[#ADD8E6] text-[#00246B]"><SiWearos className="size-10" /></div> */}
+                <div className="rounded-full bg-yellow-200 text-gray-600"><SiWearos className="size-10" /></div>
                 <div className="font-extrabold text-black whitespace-nowrap">E-Commerce</div>
             </div>
 
-            <ProductSearch 
-                onSuccess={(data: JSONObject[]) => onSearchSuccess(data) }
-                onError={(msg: string) => onSearchError(msg)}  />
+
+            <div className="flex mr-3 space-x-10 justify-center items-center">
+                <div className="flex-1">
+                    <ProductSearch 
+                    onSuccess={(data: JSONObject[]) => onSearchSuccess(data) }
+                    onError={(msg: string) => onSearchError(msg)}  />
+                </div>
+                <button className="px-5 py-2 rounded-md bg-mustard-yellow">Login</button>
+            </div>
+
         </header>
     )
 }
