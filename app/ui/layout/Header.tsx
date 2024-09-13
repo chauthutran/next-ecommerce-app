8ea3b213bd45dae3f9645100a96ad53c335ecef3
chauthutran <cthutran@gmail.com>
2024-09-13 11:45:07 +0900

    Update Header.tsx

--- app/ui/layout/Header.tsx
+++ app/ui/layout/Header.tsx
@@ -5,6 +5,7 @@ import ProductSearch from "../product/ProductSearchInput";
 import { useCurrentPage } from "@/contexts/MainUiContext";
 import * as Constant from "@/lib/constants";
 import { SiReactquery } from "react-icons/si";
+import { LuBaggageClaim } from "react-icons/lu";
 
 
 export default function Header() {
@@ -15,7 +16,7 @@ export default function Header() {
         <header className="grid grid-cols-1 md:grid-cols-2 px-4 py-3 border-b-2 border-gray-200 gap-4">
             
             <div className="text-2xl flex space-x-3 cursor-pointer items-center" onClick={() => setCurrentPage(Constant.PAGE_HOME)}>
-                <div className="rounded-full pt-1 bg-yellow-100 text-"><SiReactquery className="size-10" /></div>
+                <div className="rounded-full pt-1 bg-yellow-100 text-"><LuBaggageClaim className="size-10" /></div>
                 <div className="font-extrabold text-black whitespace-nowrap">E-Commerce</div>
             </div>
 
