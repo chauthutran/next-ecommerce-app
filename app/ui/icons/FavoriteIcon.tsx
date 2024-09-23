9a58a719f6f28a0bce050aa78c5425e6a561c6af
chauthutran <cthutran@gmail.com>
2024-09-24 00:34:30 +0900

    Update FavoriteIcon.tsx

--- app/ui/icons/FavoriteIcon.tsx
+++ app/ui/icons/FavoriteIcon.tsx
@@ -10,7 +10,7 @@ export default function FavoriteIcon() {
 
             {/* Badge */}
             <span className="absolute top-0 right-0 inline-flex items-center justify-center px-1 text-xs font-bold leading-none text-white bg-red-500 rounded-full transform translate-x-1/2 -translate-y-1/2">
-                3
+                0
             </span>
         </div>
         </>
